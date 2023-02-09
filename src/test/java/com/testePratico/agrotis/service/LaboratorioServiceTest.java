9e32d66fe94aa25e8633a230c02efed3d70cda27
helde <helderrodrigues37@gmail.com>
2023-02-09 18:27:06 -0300

    Adicionando teste de erro ao salvar um laboratorio ja existente

--- src/test/java/com/testePratico/agrotis/service/LaboratorioServiceTest.java
+++ src/test/java/com/testePratico/agrotis/service/LaboratorioServiceTest.java
@@ -1,5 +1,6 @@
 package com.testePratico.agrotis.service;
 
+import com.testePratico.agrotis.exception.RegraNegocioException;
 import com.testePratico.agrotis.model.Laboratorio;
 import com.testePratico.agrotis.repository.LaboratorioRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,6 +25,7 @@ public class LaboratorioServiceTest {
     private LaboratorioRepository laboratorioRepository;
 
     private Laboratorio laboratorio1;
+    private Optional<Laboratorio> optional;
 
     @BeforeEach
     void setUp() {
@@ -40,7 +44,20 @@ public class LaboratorioServiceTest {
 
     }
 
+    @Test
+    public void erroAoSalvar(){
+        Mockito.when(laboratorioRepository.findByNome(Mockito.anyString())).thenReturn(optional);
+
+        try {
+            laboratorioService.salvar(laboratorio1);
+        } catch (Exception e){
+            assertEquals(RegraNegocioException.class, e.getClass());
+            assertEquals("O Laboratotio " + laboratorio1.getNome() + " já está cadastrado", e.getMessage());
+        }
+    }
+
     public void iniciarObjetos(){
         laboratorio1 = new Laboratorio(1L, "teste1");
+        optional = Optional.of(new Laboratorio(2L, "teste1"));
     }
 }
