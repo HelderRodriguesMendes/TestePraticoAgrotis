fe42ac2ec9e35c2157b1041fb5d78d633522a945
helde <helderrodrigues37@gmail.com>
2023-02-09 00:12:38 -0300

    criando repository de laboratorio

---
+++ src/main/java/com/testePratico/agrotis/repository/LaboratorioRepository.java
@@ -0,0 +1,10 @@
+package com.testePratico.agrotis.repository;
+
+import com.testePratico.agrotis.model.Laboratorio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LaboratorioRepository extends JpaRepository<Laboratorio, Long> {
+
+}
